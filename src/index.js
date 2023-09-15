e4fd9e89dcc9a92e41a5b7f064a65c9e6dbfce5b
Owen Liang <owenliang3139@gmail.com>
2023-09-15 14:35:50 -0400

    Tutorial Finished

--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import React, { useState } from "react";
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function StudentInfo(props) {
+  return (
+    <div>
+      <div>
+        {props.lastName}, {props.firstName}
+      </div>
+      <ul>
+        <li>
+          <strong>ID:</strong> {props.sId}
+        </li>
+        <li>
+          <strong>School:</strong> {props.school}
+        </li>
+        <li>
+          <strong>Major:</strong> {props.major}
+        </li>
+      </ul>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const studentList = [
+  {
+    firstName: "Misty",
+    lastName: "Knight",
+    sId: "234",
+    school: "Queens College",
+    major: "Law",
+  },
+  {
+    firstName: "Jessica",
+    lastName: "Jones",
+    sId: "434",
+    school: "Brooklyn College",
+    major: "CS",
+  },
+  {
+    firstName: "Colleen",
+    lastName: "Wing",
+    sId: "233",
+    school: "Queens College",
+    major: "CS",
+  },
+  {
+    firstName: "Dare",
+    lastName: "Devil",
+    sId: "876",
+    school: "CCNY",
+    major: "Law",
+  },
+  {
+    firstName: "Luke",
+    lastName: "Cage",
+    sId: "323",
+    school: "CCNY",
+    major: "Math",
+  },
+];
+
+function ClassList() {
+  return (
+    <div>
+      <h1 className="red-text">Welcome to CTP</h1>
+      <p>List of Students</p>
+      {studentList.map((student) => (
+        <StudentInfo {...student} key={student.sId} />
+      ))}
+    </div>
+  );
+}
+
+function CountApp() {
+  const [numClicks, setNumClicks] = useState(0);
+
+  const handleClick = (event) => {
+    setNumClicks(numClicks + 1);
+  };
+
+  return (
+    <div>
+      <p>The button has been clicked {numClicks} times</p>
+      <button onClick={handleClick}>Click this button</button>
+    </div>
+  );
+}
+
+const htmlContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(htmlContainer);
+root.render(<CountApp />);
+//root.render(<ClassList />)
